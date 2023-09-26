158e0d20087a403c84afa2b977c40a678c38b123
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-09-25 21:12:42 -0300

    base para Prospectar clientes

---
+++ prospect-back/src/main/java/com/example/prospect/ProspectApplication.java
@@ -0,0 +1,13 @@
+package com.example.prospect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProspectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProspectApplication.class, args);
+	}
+
+}
