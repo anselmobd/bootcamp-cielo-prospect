77e93e330f2d2b971f14d0fe4aed4f4a204df254
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-09-26 17:33:15 -0300

    getPessoaFisicaByCpf pretendo que seja útil ao processar a fila

--- prospect-back/src/main/java/com/example/prospect/repository/PessoaFisicaRepository.java
+++ prospect-back/src/main/java/com/example/prospect/repository/PessoaFisicaRepository.java
@@ -4,7 +4,11 @@ import com.example.prospect.entity.PessoaFisica;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PessoaFisicaRepository
         extends JpaRepository<PessoaFisica, Long> {
+
+    Optional<PessoaFisica> getPessoaFisicaByCpf(String cpf);
 }
