fd85a57689722a7ee5b646e4c2faf70c15760e11
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-09-26 21:29:03 -0300

    Corrigindo HttpStatus da PessoaBadRequestException

--- prospect-back/src/main/java/com/example/prospect/exception/PessoaBadRequestException.java
+++ prospect-back/src/main/java/com/example/prospect/exception/PessoaBadRequestException.java
@@ -3,7 +3,7 @@ package com.example.prospect.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.BAD_REQUEST)  // 406
+@ResponseStatus(value=HttpStatus.NOT_ACCEPTABLE)  // 406
 public class PessoaBadRequestException extends RuntimeException {
     public PessoaBadRequestException(String message) {
         super(message);
