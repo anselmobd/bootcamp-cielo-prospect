0e4d45a0f5367452d84f15e18b3070181a7fab6b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-09-26 21:19:40 -0300

    Bug: PessoaConflictException e PessoaBadRequestException deveriam estar nos seus arquivos

--- prospect-back/src/main/java/com/example/prospect/exception/PessoaBadRequestException.java
+++ prospect-back/src/main/java/com/example/prospect/exception/PessoaBadRequestException.java
@@ -3,9 +3,9 @@ package com.example.prospect.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND)  // 404
-public class PessoaNotFoundException extends RuntimeException {
-    public PessoaNotFoundException(String message) {
+@ResponseStatus(value= HttpStatus.BAD_REQUEST)  // 406
+public class PessoaBadRequestException extends RuntimeException {
+    public PessoaBadRequestException(String message) {
         super(message);
     }
     // ...
--- prospect-back/src/main/java/com/example/prospect/exception/PessoaConflictException.java
+++ prospect-back/src/main/java/com/example/prospect/exception/PessoaConflictException.java
@@ -3,9 +3,9 @@ package com.example.prospect.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value= HttpStatus.NOT_FOUND)  // 404
-public class PessoaNotFoundException extends RuntimeException {
-    public PessoaNotFoundException(String message) {
+@ResponseStatus(value= HttpStatus.CONFLICT)  // 409
+public class PessoaConflictException extends RuntimeException {
+    public PessoaConflictException(String message) {
         super(message);
     }
     // ...
