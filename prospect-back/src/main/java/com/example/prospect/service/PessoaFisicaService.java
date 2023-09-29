4792e0daf54534fc7840f5e87204878c98141a30
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-09-28 22:03:40 -0300

    addPessoaFisica já adicionando na fila

--- prospect-back/src/main/java/com/example/prospect/service/PessoaFisicaService.java
+++ prospect-back/src/main/java/com/example/prospect/service/PessoaFisicaService.java
@@ -5,10 +5,10 @@ import com.example.prospect.entity.input.EntradaPessoaFisica;
 import com.example.prospect.exception.PessoaConflictException;
 import com.example.prospect.exception.PessoaNotFoundException;
 import com.example.prospect.repository.PessoaFisicaRepository;
+import com.example.prospect.util.FilaPessoaVersao;
+import com.example.prospect.util.PessoaVersao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -16,10 +16,12 @@ import java.util.Optional;
 @Service
 public class PessoaFisicaService {
 
+    private final FilaPessoaVersao filaPessoaVersao;
     private final PessoaFisicaRepository pessoaFisicaRepository;
 
     @Autowired
-    public PessoaFisicaService(PessoaFisicaRepository pessoaFisicaRepository) {
+    public PessoaFisicaService(FilaPessoaVersao filaPessoaVersao, PessoaFisicaRepository pessoaFisicaRepository) {
+        this.filaPessoaVersao = filaPessoaVersao;
         this.pessoaFisicaRepository = pessoaFisicaRepository;
     }
 
@@ -42,7 +44,13 @@ public class PessoaFisicaService {
         if (exists) {
             throw new PessoaConflictException("Já existe pessoa física com CPF: " + cpf);
         }
-        return this.pessoaFisicaRepository.save(pessoaFisica);
+        PessoaFisica savedPessoaFisica = this.pessoaFisicaRepository.save(pessoaFisica);
+        PessoaVersao pessoaVersao = new PessoaVersao(
+                savedPessoaFisica.getCpf(),
+                savedPessoaFisica.getVersao()
+        );
+        filaPessoaVersao.inFila(pessoaVersao);
+        return savedPessoaFisica;
     }
 
     public void deletePessoaFisica(long id) throws PessoaNotFoundException {
