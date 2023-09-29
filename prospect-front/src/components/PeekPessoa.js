5583827c59ecfffd7acb70b8754e2816eaf6c72b
Anselmo Blanco Dominguez <anselmo.blanco.dominguez@gmail.com>
2023-09-29 19:19:02 -0300

    evoluindo a tela /prospecta_cliente

--- prospect-front/src/components/PeekPessoa.js
+++ prospect-front/src/components/PeekPessoa.js
@@ -6,7 +6,7 @@ function PeekPessoa() {
         <Card style={{ width: '24rem' }}>
             <ListGroup variant="flush">
                 <ListGroup.Item>Cadastro: <b>12345678900</b></ListGroup.Item>
-                <ListGroup.Item>Versão: <b>2023-09-08 45:54:87.4324</b></ListGroup.Item>
+                {/*<ListGroup.Item>Versão: <b>2023-09-08 45:54:87.4324</b></ListGroup.Item>*/}
             </ListGroup>
         </Card>
     );
--- prospect-front/src/components/ProspectaCliente.js
+++ prospect-front/src/components/ProspectaCliente.js
@@ -1,4 +1,5 @@
 import React from "react";
+import axios from "axios";
 import './ProspectaCliente.css'
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
@@ -7,21 +8,52 @@ import PeekPessoa from "./PeekPessoa";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import ListGroup from "react-bootstrap/ListGroup";
+import {json} from "react-router-dom";
 
 class ProspectaCliente extends React.Component {
 
     constructor(props) {
         super(props);
         this.state = {
-            id: 1,
+            id: '1',
             versao: '2',
-            cnpj: '',
-            razao_social: '22',
-            cpf: '3',
-            mcc: '4',
-            nome: '5',
-            email: '6'
+            cnpj: '3',
+            razao_social: '4',
+            cpf: '5',
+            mcc: '6',
+            nome: '7',
+            email: '8'
         };
+    }
+
+    componentDidMount() {
+        this.initState();
+    }
+
+    initState = () => {
+        this.setState(
+            {
+                id: '',
+                versao: '',
+                cnpj: '',
+                razao_social: '',
+                cpf: '',
+                mcc: '',
+                nome: '',
+                email: ''
+            }
+        );
+    }
+
+    fetchPessoaDados = () => {
+        this.initState();
+        axios.get("http://localhost:8080/api/v1/pessoa_fisica")
+            .then(res => {
+                let dados = res.data;
+                if (dados.length > 0) {
+                    this.setState(dados[0]);
+                }
+            });
     }
 
     render() {
@@ -36,7 +68,7 @@ class ProspectaCliente extends React.Component {
                     </Col>
                     <Col className="titulo_central">
                         <p>&nbsp;</p>
-                        <Button variant="primary" size="lg">
+                        <Button variant="primary" size="lg" onClick={this.fetchPessoaDados}>
                             Prospecta
                         </Button>
                     </Col>
@@ -45,8 +77,8 @@ class ProspectaCliente extends React.Component {
                         <p>Pessoa jurídica retirada da fila</p>
                         <Card style={{ width: '24rem' }}>
                             <ListGroup variant="flush">
-                                <ListGroup.Item>ID: <b>{this.state.id}</b></ListGroup.Item>
-                                <ListGroup.Item>Versão: <b>{this.state.versao}</b></ListGroup.Item>
+                                {/*<ListGroup.Item>ID: <b>{this.state.id}</b></ListGroup.Item>*/}
+                                {/*<ListGroup.Item>Versão: <b>{this.state.versao}</b></ListGroup.Item>*/}
                                 <ListGroup.Item>CNPJ: <b>{this.state.cnpj}</b></ListGroup.Item>
                                 <ListGroup.Item>Razão Social: <b>{this.state.razao_social}</b></ListGroup.Item>
                                 <ListGroup.Item>MCC: <b>{this.state.mcc}</b></ListGroup.Item>
@@ -59,8 +91,8 @@ class ProspectaCliente extends React.Component {
                         <p>Pessoa física retirada da fila</p>
                         <Card style={{ width: '24rem' }}>
                             <ListGroup variant="flush">
-                                <ListGroup.Item>ID: <b>{this.state.id}</b></ListGroup.Item>
-                                <ListGroup.Item>Versão: <b>{this.state.versao}</b></ListGroup.Item>
+                                {/*<ListGroup.Item>ID: <b>{this.state.id}</b></ListGroup.Item>*/}
+                                {/*<ListGroup.Item>Versão: <b>{this.state.versao}</b></ListGroup.Item>*/}
                                 <ListGroup.Item>CPF: <b>{this.state.cpf}</b></ListGroup.Item>
                                 <ListGroup.Item>MCC: <b>{this.state.mcc}</b></ListGroup.Item>
                                 <ListGroup.Item>Nome: <b>{this.state.nome}</b></ListGroup.Item>
